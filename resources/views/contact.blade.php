bfcb17d43d3e6b00d4b3747d974bca43c4fb3c20
Richardo06 <alimananirina06@gmail.com>
2025-06-18 12:16:07 +0300

    conatct

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,70 +16,46 @@
       </div><!-- End Section Title -->
 
       <div class="container" data-aos="fade" data-aos-delay="100">
-
         <div class="row gy-4">
-
-          <div class="col-lg-4">
-            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
-              <i class="bi bi-geo-alt flex-shrink-0"></i>
-              <div>
-                <h3>Address</h3>
-                <p>Lot III N 01 CB Fiadanana, Tananarive, MADAGASCAR</p>
-              </div>
-            </div><!-- End Info Item -->
-
-            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
-              <i class="bi bi-telephone flex-shrink-0"></i>
-              <div>
-                <h3>Call Us</h3>
-                <p>+261 38 77 256 25 ou +261 33 30 121 77</p>
-              </div>
-            </div><!-- End Info Item -->
-
-            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
-              <i class="bi bi-envelope flex-shrink-0"></i>
-              <div>
-                <h3>Email Us</h3>
-                <p>ralaivaorichardo@gmail.com</p>
-              </div>
-            </div><!-- End Info Item -->
-
+          <!-- Adresse et Téléphone côte à côte -->
+          <div class="col-lg-6 mb-4">
+        <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
+          <i class="bi bi-geo-alt flex-shrink-0"></i>
+          <div>
+            <h3>Address</h3>
+            <p>Lot III N 01 CB Fiadanana, Tananarive, MADAGASCAR</p>
           </div>
-
-          <div class="col-lg-8">
-            <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
-              <div class="row gy-4">
-
-                <div class="col-md-6">
-                  <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
-                </div>
-
-                <div class="col-md-6 ">
-                  <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
-                </div>
-
-                <div class="col-md-12">
-                  <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
-                </div>
-
-                <div class="col-md-12">
-                  <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
-                </div>
-
-                <div class="col-md-12 text-center">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">Your message has been sent. Thank you!</div>
-
-                  <button type="submit">Send Message</button>
-                </div>
-
-              </div>
-            </form>
-          </div><!-- End Contact Form -->
-
         </div>
-
+          </div>
+          <div class="col-lg-6 mb-4">
+        <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
+          <i class="bi bi-telephone flex-shrink-0"></i>
+          <div>
+            <h3>Call Us</h3>
+            <p>+261 38 77 256 25 ou +261 33 30 121 77</p>
+          </div>
+        </div>
+          </div>
+          <!-- Email en dessous sur toute la largeur -->
+          <div class="col-lg-6 mb-4">
+        <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+          <i class="bi bi-envelope flex-shrink-0"></i>
+          <div>
+            <h3>Email Us</h3>
+            <p>ralaivaorichardo@gmail.com</p>
+          </div>
+        </div>
+          </div>
+          <div class="col-lg-6 mb-4">
+        <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="500">
+          <i class="bi bi-whatsapp flex-shrink-0"></i>
+          <div>
+            <h3>WhatsApp</h3>
+            <p>+261 38 77 256 25</p>
+          </div>
+        </div>
+          </div>
+        </div>
       </div>
 
     </section><!-- /Contact Section -->
